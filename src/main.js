fbfe0c1ad72150a8ce2c3a69b1573029bf920e50
germiona <violetta.ashirova@gmail.com>
2019-11-25 19:38:13 +0300

    module1-task2

--- src/main.js
+++ src/main.js
@@ -41,7 +41,8 @@ const getFilters = () => {
       ${filters.map((filter) => {
         return `
           <input type="radio" id="filter__${filter.name.toLowerCase()}" class="filter__input visually-hidden" name="filter" checked="">
-          <label for="filter__${filter.name.toLowerCase()}}" class="filter__label">${filter.name} <span class="filter__all-count">${filter.num}</span></label>`;
+          <label for="filter__${filter.name.toLowerCase()}}" class="filter__label">${filter.name} <span class="filter__all-count">${filter.num}</span></label>
+        `;
       }).join(``)};
     <section>
   `;
