02c0a1fbb6b6a89ba1d563cc44c6157271ec8a6c
germiona <violetta.ashirova@gmail.com>
2019-11-30 20:03:17 +0300

    fixes

--- src/main.js
+++ src/main.js
@@ -1,13 +1,14 @@
 import {filters} from "./constants";
 
-import {getMenu} from "./components/menu";
 import {getFilters} from "./components/filters";
 import {getSort} from "./components/sorting";
 import {getCard} from "./components/card";
 import {getCardEdit} from "./components/card-edit";
 import {getSearch} from "./components/search";
 import {getLoad} from "./components/load-more";
+import {getMenu} from "./components/menu";
 import {getMenuWrapper} from "./components/menu-wrapper";
+
 
 const main = document.querySelector(`.main`);
 const mainControl = document.querySelector(`.main__control`);
@@ -31,7 +32,7 @@ const getContent = () => {
   `;
 };
 
-mainControl.insertAdjacentHTML(`beforeend`, getMenuWrapper(getMenu()));
+mainControl.insertAdjacentHTML(`beforeend`, getMenuWrapper(getMenu));
 main.insertAdjacentHTML(`beforeend`, getSearch());
 main.insertAdjacentHTML(`beforeend`, getFilters(filters));
 main.insertAdjacentHTML(`beforeend`, getContent());
