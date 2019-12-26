32c88e53b279d7e27d7855ed397177f188325fd7
germiona <violetta.ashirova@gmail.com>
2019-12-26 19:53:12 +0300

    module5-task1

--- src/components/sorting.js
+++ src/components/sorting.js
@@ -1,6 +1,5 @@
 import AbstractComponent from './abstract-component.js';
 
-
 const createSortTemplate = () => {
   return `<div class="board__filter-list">
         <a href="#" class="board__filter">SORT BY DEFAULT</a>
